823164d2b23b4991a732e7bbe855e6eac450c0c3
Vladimir Baltiev <v.golem2288@gmail.com>
2023-11-25 18:05:07 +0600

    added important components into backend. Also, added model User and routers /GET /POST for it.

---
+++ backend/fixtures.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const {nanoid} = require('nanoid');
+const config = require('./config');
+
+const User = require('./models/User');
+
+const run = async () => {
+    await mongoose.connect(config.mongo.db);
+
+    const collections = await mongoose.connection.db.listCollections().toArray();
+
+    for (const coll of collections) {
+        await mongoose.connection.db.dropCollection(coll.name);
+    }
+
+    await User.create({
+        gmail: 'vovvkka0587@gmail.com',
+        username: "vovvkka",
+        password: 'admin',
+        token: nanoid(),
+        role: 'admin',
+    });
+
+    await mongoose.connection.close();
+};
+
+run().catch(console.error);
